10cf91b13519157ae67917ddbd7045d73d582564
Angelito <57669628+aavega2018@users.noreply.github.com>
2026-09-17 16:55:38 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -14,15 +14,85 @@ const SITE_DATA = {
   },
   "categorias": [
     "Artículos para Bebés"
+    "Bebés"
+"Bicicletas y Patinetas"
+"Carros de Control Remoto"
+"Carros, Rastras y Camiones"
+"Castillos"
+"Juguetes Didácticos"
+"Juguetes Musicales"
+"Libros de Cuentos"
+"Muñecas"
+"Pelotas y Juguetes de Playa"
+"Pistolas"
+"Set de Armas"
+"Set de Cocina, Médico y de Uñas"
+"Set de Corona y de Tacones"
+"Set de Dino, Animales y de Herramientas"
+"Set de Maquillaje y de Armar Pulsas"
   ],
   "productos": [
     {
-      "id": "set-de-regalos-para-bebes-1789670860884",
+      "id": "00011",
       "nombre": "Set de Regalos para Bebés",
       "precio": 3500,
-      "imagen": "img/1.jpeg",
+      "imagen": "img/11.jpeg",
       "categoria": "Artículos para Bebés",
       "descripcion": "Set de Regalos para Bebés",
+      "activo": true
+    }
+    {
+      "id": "00012",
+      "nombre": "Sonares para Bebés",
+      "precio": 2500,
+      "imagen": "img/12.jpeg",
+      "categoria": "Artículos para Bebés",
+      "descripcion": "Sonares para Bebés",
+      "activo": true
+    }
+    {
+      "id": "00013",
+      "nombre": "Happy Horse",
+      "precio": 3500,
+      "imagen": "img/13.jpeg",
+      "categoria": "Artículos para Bebés",
+      "descripcion": "El regalo Perfecto para hacer reir a tu pequeña",
+      "activo": true
+    }
+    {
+      "id": "00014",
+      "nombre": "Combo Maquillaje para Bebés",
+      "precio": 2500,
+      "imagen": "img/14.jpeg",
+      "categoria": "Artículos para Bebés",
+      "descripcion": "Combo maquillaje para bebé",
+      "activo": true
+    }
+    {
+      "id": "00015",
+      "nombre": "Muñes de Gomas que Pitan",
+      "precio": 1500,
+      "imagen": "img/15.jpeg",
+      "categoria": "Artículos para Bebés",
+      "descripcion": "Muñes de Bomas que pitan con amor",
+      "activo": true
+    }
+    {
+      "id": "00016",
+      "nombre": "Jaboneras de Viajes",
+      "precio": 1000,
+      "imagen": "img/16.jpeg",
+      "categoria": "Artículos para Bebés",
+      "descripcion": "Jaboneras de Viajes para Bebés con un diseño Antiderrames, Colores Vibrantes, Compactar y Ligeras",
+      "activo": true
+    }
+    {
+      "id": "00017",
+      "nombre": "Set de Alimentación",
+      "precio": 2500,
+      "imagen": "img/17.jpeg",
+      "categoria": "Artículos para Bebés",
+      "descripcion": "El Kit perfecto para su Alimentación",
       "activo": true
     }
   ]
