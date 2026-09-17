a45b285509c17da32a7dcf3e190fea65ab9a52fc
Angelito <57669628+aavega2018@users.noreply.github.com>
2026-09-17 18:03:20 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -95,6 +95,186 @@ const SITE_DATA = {
       "descripcion": "El Kit perfecto para su Alimentación",
       "activo": true
     },
+    {
+      "id": "00018",
+      "nombre": "Cadenitas de Tete",
+      "precio": 1000,
+      "imagen": "img/18.jpeg",
+      "categoria": "Artículos para Bebés",
+      "descripcion": "Seguridad y Estilo para tu Bebé",
+      "activo": true
+    },
+    {
+      "id": "00021",
+      "nombre": "Coche con Bebé que Llora",
+      "precio": 8000,
+      "imagen": "img/21.jpeg",
+      "categoria": "Bebés",
+      "descripcion": "Coche con Bebé que Llora ¡¡De Verdad!!",
+      "activo": true
+    },
+    {
+      "id": "00022",
+      "nombre": "Muñeca que Llora",
+      "precio": 5500,
+      "imagen": "img/22.jpeg",
+      "categoria": "Bebés",
+      "descripcion": "Muñeca que Llora",
+      "activo": true
+    },
+    {
+      "id": "00023",
+      "nombre": "Bebés y Muñecas Lloronas",
+      "precio": 3000,
+      "imagen": "img/23.jpeg",
+      "categoria": "Bebés",
+      "descripcion": "Bebés y Muñecas Lloronas (Son Pequeños)",
+      "activo": true
+    },
+    {
+      "id": "00024",
+      "nombre": "Muñeca para Bebé",
+      "precio": 7000,
+      "imagen": "img/24.jpeg",
+      "categoria": "Bebés",
+      "descripcion": "Muñecas para Bebés Hembras",
+      "activo": true
+    },
+    {
+      "id": "00031",
+      "nombre": "Carriolas para Niños(3-7 años)",
+      "precio": 20000,
+      "imagen": "img/31.jpeg",
+      "categoria": "Bicicletas y Patinetas",
+      "descripcion": "Carriolas para Niños(3-7 años) Altura 7cm Largo 61cm",
+      "activo": true
+    },
+     {
+      "id": "00032",
+      "nombre": "Patinetas",
+      "precio": 12000,
+      "imagen": "img/32.jpeg",
+      "categoria": "Bicicletas y Patinetas",
+      "descripcion": "Patinetas para ambas edades",
+      "activo": true
+    },
+    {
+      "id": "00041",
+      "nombre": "Carro de Control Remoto",
+      "precio": 15000,
+      "imagen": "img/41.jpeg",
+      "categoria": "Carros de Control Remoto",
+      "descripcion": "Carro de Control Remoto (Baterías de Lítio y Control y Control Volante)",
+      "activo": true
+    },
+    {
+      "id": "00042",
+      "nombre": "Carro lamborghini con Luces",
+      "precio": 7000,
+      "imagen": "img/42.jpeg",
+      "categoria": "Carros de Control Remoto",
+      "descripcion": "Carro lamborghini con Luces (32cm)",
+      "activo": true
+    },
+    {
+      "id": "00043",
+      "nombre": "Jeep con Control Remoto",
+      "precio": 7000,
+      "imagen": "img/43.jpeg",
+      "categoria": "Carros de Control Remoto",
+      "descripcion": "Jeet con Control Remoto",
+      "activo": true
+    },
+    {
+      "id": "00044",
+      "nombre": "Ford mustang con Control Remoto",
+      "precio": 7000,
+      "imagen": "img/44.jpeg",
+      "categoria": "Carros de Control Remoto",
+      "descripcion": "Ford mustang con Control Remoto",
+      "activo": true
+    },
+    {
+      "id": "00051",
+      "nombre": "Camión de la Contrucción",
+      "precio": 2500,
+      "imagen": "img/51.jpeg",
+      "categoria": "Carros, Rastras y Camiones",
+      "descripcion": "Contruye tus aventuras",
+      "activo": true
+    },
+    {
+      "id": "00052",
+      "nombre": "Cargador Frontal (o pala cargadora)",
+      "precio": 2500,
+      "imagen": "img/52.jpeg",
+      "categoria": "Carros, Rastras y Camiones",
+      "descripcion": "Contruye tu imaginación",
+      "activo": true
+    },
+    {
+      "id": "00053",
+      "nombre": "Camión Montruo de Construcción (3 Modelos)",
+      "precio": 1500,
+      "imagen": "img/53.jpeg",
+      "categoria": "Carros, Rastras y Camiones",
+      "descripcion": "Tu propio camión montruo de construcción",
+      "activo": true
+    },
+    {
+      "id": "00054",
+      "nombre": "Jeep Todoterreno",
+      "precio": 6000,
+      "imagen": "img/54.jpeg",
+      "categoria": "Carros, Rastras y Camiones",
+      "descripcion": "Jeep Todoterreno",
+      "activo": true
+    },
+    {
+      "id": "00055",
+      "nombre": "Carros de Patrullas",
+      "precio": 800,
+      "imagen": "img/55.jpeg",
+      "categoria": "Carros, Rastras y Camiones",
+      "descripcion": "Carros de Patrullas",
+      "activo": true
+    },
+    {
+      "id": "00056",
+      "nombre": "Rastras Montacargas (Trailers)",
+      "precio": 3500,
+      "imagen": "img/56.jpeg",
+      "categoria": "Carros, Rastras y Camiones",
+      "descripcion": "Rastras Montacargas (Super TRUCK)",
+      "activo": true
+    },
+    {
+      "id": "00057",
+      "nombre": "Lanchas Rápidas",
+      "precio": 6000,
+      "imagen": "img/57.jpeg",
+      "categoria": "Carros, Rastras y Camiones",
+      "descripcion": "Receing Speed Boat",
+      "activo": true
+    },
+    {
+      "id": "00058",
+      "nombre": "Carro Bombero Cisterna",
+      "precio": 5000,
+      "imagen": "img/58.jpeg",
+      "categoria": "Carros, Rastras y Camiones",
+      "descripcion": "Carro Bombero Cisterna (Hecha Agua)",
+      "activo": true
+    },
+    {
+      "id": "00059",
+      "nombre": "Carro de Carreras",
+      "precio": 7000,
+      "imagen": "img/59.jpeg",
+      "categoria": "Carros, Rastras y Camiones",
+      "descripcion": "Carro de Carreras",
+      "activo": true
+    },
   ]
 };
 
